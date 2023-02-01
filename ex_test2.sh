6d1ab322ed172be56fb9d9cf9f2c3ffaaa3d2dfd
sebDtSci <sebDtSci@users.noreply.github.com>
2023-02-01 12:36:28 +0100

    git pour exo1

---
+++ ex_test2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+dir_count=0 
+exec_count=0 
+f_non_count=0
+for i in *
+do 
+
+	if [ -d "$i" ]
+	then ((dir_count++)) 
+
+	elif [ -x "$i" ]
+	then ((exec_count++)) 
+	elif [ -f "$i" ]
+	then ((f_non_count++)) 
+	
+	fi 
+done 
+
+echo "vous avez $exec_count fichier executables, $dir_count dossier et $f_non_count fichier non executables."
